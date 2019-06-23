4accd2b2062ce58bac6dbc55308d0d5a11c68fe5
Paul Harvey <jhebus@gmail.com>
2019-06-24 08:43:54 +0900

    Update echoToCml.py

--- echoToCml.py
+++ echoToCml.py
@@ -2,14 +2,14 @@ from socket import *
 import sys
 
 #HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+#PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 def echo_to_cml(port):
     print('Listening on port %d' % (port))
     in_sock = socket(AF_INET, SOCK_STREAM)
     in_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    #in_sock.bind(('', port))
-    in_sock.bind(('', PORT))
+    in_sock.bind(('', port))
+    #in_sock.bind(('', PORT))
     in_sock.listen(5)
 
     while True:
